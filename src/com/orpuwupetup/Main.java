d28c56b59c7390c616ccc22246e8fd93ffcea928
Cezary <cezarygraban@gmail.com>
2020-02-22 12:35:16 +0100

    Initial commit from zjazd 2

---
+++ src/com/orpuwupetup/Main.java
@@ -0,0 +1,16 @@
+package com.orpuwupetup;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double foo = 20, bar = 40, baz = (foo+bar)*100;
+
+        double mogulo;
+        boolean isZero;
+
+        if (isZero = (mogulo = baz % 40.00d) > 0) {
+            System.out.println(mogulo);
+        }
+    }
+}
